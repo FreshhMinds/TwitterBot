12310bf0f52d69ddd0c0613eb0aa6cd5f45307a3
Savan Sutariya <savanpatel5248@gmail.com>
2022-08-16 22:56:34 +0530

    Deploy attempt 1

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import tweepy
 import time
-from private import *
 from Modules.NDaysOfCode import NDaysOfCode
+import os
+FREQUENCY = int(os.getenv('FREQUENCY'))
+API_Key = os.getenv('API_KEY')
+API_Key_Secret = os.getenv('API_KEY_SECRET')
+access_token = os.getenv('ACCESS_TOKEN')
+acces_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
 # auth = tweepy.OAuth2UserHandler()
 
 auth = tweepy.OAuthHandler(API_Key, API_Key_Secret,access_token, acces_token_secret)
@@ -11,5 +16,5 @@ p1 = NDaysOfCode(api,100)
 
 while(True):
     p1.quote_retweet()
-    print("Iterating..")
-    time.sleep(60)
+    print("Sleeping for " + str(FREQUENCY) + " seconds")
+    time.sleep(FREQUENCY)
